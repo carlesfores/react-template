8dbd6e3231b175d5333d0bc151dd71033aa40ff3
cfores <carles.fores@zitrogames.com>
2021-03-15 16:09:51 +0100

    Add React Routes and few test components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+      <Router>
+        <div className="app-wrapper">
+          <header>
+            <Nav/>
+          </header>
+          <Switch>
+            <main>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
+            </main>
+          </Switch>
+          <footer>
+            <h3>page footer</h3>
+          </footer>
+        </div>
+      </Router>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Nav.css';
+
+function Nav() {
+    return (
+        <div className="Nav">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+        </div>
+    );
+}
+
+export default Nav;
